69d07ca41dd479023a036b2b7831932de8f5a869
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-05-30 17:42:22 +0200

    Implement items API

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'site#index'
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :create, :destroy, :update]
+    end
+  end
+end
